05faa7b0834383e4ae7ce440a61dd85436489f38
CodeDOJO <abhishek0387@gmail.com>
2017-05-01 12:29:32 -0400

    check in

---
+++ Test/Project1/src/project1/Class1.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class Class1 {
+    public Class1() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is to show how to check in JIRA");
+    }
+}
