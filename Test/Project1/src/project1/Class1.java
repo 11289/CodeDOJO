5e03e06e562b988ad80c2d3c482dcbe124cebb61
CodeDOJO <abhishek0387@gmail.com>
2017-05-05 15:42:14 -0400

    commit

--- Test/Project1/src/project1/Class1.java
+++ Test/Project1/src/project1/Class1.java
@@ -8,5 +8,6 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("This is the example of first check in");
 		System.out.println("This is the example of second check in");
+        System.out.println("Thsi is third line of addition");
     }
 }
